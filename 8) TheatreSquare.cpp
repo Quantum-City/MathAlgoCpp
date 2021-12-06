d8f4608c70311d9901ab4faed3795221598cedba
Quantum City <69152181+Quantum-City@users.noreply.github.com>
2021-12-06 14:20:09 +0530

    Create 8) TheatreSquare.cpp

---
+++ 8) TheatreSquare.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m, a;
+    long long int i, j;
+    cin >> n >> m >> a;
+    i = n / a;
+    j = m / a;
+    if (i * a != n)
+        i++;
+    if (j * a != m)
+        j++;
+    cout << i * j;
+    return 0;
+}
