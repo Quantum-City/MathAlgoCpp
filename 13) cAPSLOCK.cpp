ceb59ea1782493f843a27990ae49015fbf465474
Quantum City <69152181+Quantum-City@users.noreply.github.com>
2021-12-15 14:11:51 +0530

    Create 13) cAPSLOCK.cpp

---
+++ 13) cAPSLOCK.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+char a[105];
+int main()
+{
+    int i = 1, f = 1;
+    gets(a);
+    for (; a[i];)
+        f &= (a[i++] < 'a');
+    for (i = 0; a[i];)
+        putchar(a[i++] ^ f * 32);
+    return 0;
+}
